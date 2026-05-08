585557de7219e52910a2254584a0edf734dd1b32
harah6334-spec <harah6334@gmail.com>
2026-05-08 18:05:53 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, loadEnv } from 'vite';
+import react from '@itejs/plugin-react';
+import tailwindcss from '@tailwindcss/ite';
+import { defineConfig, loadEn } from 'ite';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const en = loadEn(mode, process.cwd(), '');
 
   return {
+    root: path.resole(__dirname, '.'),
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.en.GEMINI_API_KEY': JSON.stringify(en.GEMINI_API_KEY),
     },
-    resolve: {
+    resole: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': path.resole(__dirname, './src'),
       },
     },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true', // we need this constraint inside AI Studio
+    serer: {
+      hmr: process.en.DISABLE_HMR !== 'true', // we need this constraint inside AI Studio
       port: 5173,
       proxy: {
         '/api': {
